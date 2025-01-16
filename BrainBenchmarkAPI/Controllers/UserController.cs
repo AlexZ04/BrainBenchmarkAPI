31893cbfcb0ac9eec72bc3738047bccc8a71dca9
Aleksey Zinovjev <alexz04tab2@gmail.com>
2025-01-16 23:35:59 +0700

    add connection to db

---
+++ BrainBenchmarkAPI/Controllers/UserController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Runtime.CompilerServices;
+
+namespace BrainBenchmarkAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly DbContext _dbContext;
+
+        public UserController(DbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public IActionResult Get() {
+            return Ok();
+        }
+    }
+}
--- BrainBenchmarkAPI/Program.cs
+++ BrainBenchmarkAPI/Program.cs
@@ -1,20 +1,29 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+var connection = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<DbContext>(options => options.UseNpgsql(connection));
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+using var serviceScope = app.Services.CreateScope();
+
+var context = serviceScope.ServiceProvider.GetService<DbContext>();
+
+context?.Database.Migrate();
 
 app.UseHttpsRedirection();
 
