f3231d2cecf40ef9d9d0ca379ea84432e31a586a
Aleksey Zinovjev <alexz04tab2@gmail.com>
2025-01-21 15:10:28 +0700

    add player controller

---
+++ BrainBenchmarkAPI/Controllers/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using BrainBenchmarkAPI.Data;
+using BrainBenchmarkAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace BrainBenchmarkAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlayerController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public PlayerController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("profile")]
+        public async Task<IActionResult> GetPlayerProfile([Required, FromQuery] Guid id)
+        {
+            var dbPlayer = await _context.Users.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (dbPlayer == null) return NotFound(new ResponseModel("Error", "Wrong user id!"));
+
+            var player = new PlayerInfoModel(new UserModel(dbPlayer));
+
+            return Ok(player);
+        }
+    }
+}
---
+++ BrainBenchmarkAPI/Models/PlayerInfoModel.cs
@@ -0,0 +1,12 @@
+﻿namespace BrainBenchmarkAPI.Models
+{
+    public class PlayerInfoModel
+    {
+        public UserModel MainInfo { get; set; }
+        
+        public PlayerInfoModel(UserModel info) 
+        { 
+            MainInfo = info;
+        }
+    }
+}
