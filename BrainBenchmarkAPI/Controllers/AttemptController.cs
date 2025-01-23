79f480a010a25144e248f971f8f8d0a31364bbb1
Aleksey Zinovjev <alexz04tab2@gmail.com>
2025-01-23 15:24:48 +0700

    add getting last attempts

--- BrainBenchmarkAPI/Controllers/AttemptController.cs
+++ BrainBenchmarkAPI/Controllers/AttemptController.cs
@@ -1,7 +1,12 @@
 ﻿using BrainBenchmarkAPI.Data;
+using BrainBenchmarkAPI.Filters;
+using BrainBenchmarkAPI.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.ComponentModel.DataAnnotations;
+using System.Security.Claims;
 
 namespace BrainBenchmarkAPI.Controllers
 {
@@ -17,10 +22,88 @@ namespace BrainBenchmarkAPI.Controllers
         }
 
 
+        /// <summary>
+        /// Get last 10 attempts
+        /// </summary>
+        /// <response code="200">Get list of attempts short models</response>
+        /// <response code="500">Internal server error</response>
+        [ProducesResponseType(typeof(List<AttemptShortModel>), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ResponseModel), StatusCodes.Status200OK)]
+        [HttpGet("attempt")]
+        public async Task<IActionResult> GetLastAttempts()
+        {
+            var attempts = await _context.Attempts
+                .OrderByDescending(at => at.AttemptDate)
+                .Take(10)
+                .ToListAsync();
+
+            List<AttemptShortModel> res = new List<AttemptShortModel>();
+
+            foreach (var attempt in attempts)
+            {
+                res.Add(new AttemptShortModel(attempt));
+            }
+
+            return Ok(res);
+        }
+
+
+        /// <summary>
+        /// Get last 10 attempts of user
+        /// </summary>
+        /// <response code="200">Get list of user's attempts short models</response>
+        /// <response code="404">Can't find the user</response>
+        /// <response code="500">Internal server error</response>
+        [ProducesResponseType(typeof(List<AttemptShortModel>), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ResponseModel), StatusCodes.Status200OK)]
+        [HttpGet("attempt/user")]
+        [Authorize]
+        [CheckTokenFilter]
+        public async Task<IActionResult> GetUserLastAttempts()
+        {
+            var userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+            if (userId == null) return NotFound(new ResponseModel("Error", "User not found"));
+
+            var id = new Guid(userId);
+
+            var attempts = await _context.Attempts
+                .Include(at => at.Player)
+                .OrderByDescending(at => at.AttemptDate)
+                .Where(at => at.Player.Id == id)
+                .Take(10)
+                .ToListAsync();
+
+            List<AttemptShortModel> res = new List<AttemptShortModel>();
+
+            foreach (var attempt in attempts)
+            {
+                res.Add(new AttemptShortModel(attempt));
+            }
+
+            return Ok(res);
+        }
+
+
+        /// <summary>
+        /// Get info about attempt
+        /// </summary>
+        /// <response code="200">Returns the info about the attempt</response>
+        /// <response code="404">Can't find the attempt</response>
+        /// <response code="500">Internal Server Error</response>
+        [ProducesResponseType(typeof(AttemptModel), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(void), StatusCodes.Status404NotFound)]
+        [ProducesResponseType(typeof(ResponseModel), StatusCodes.Status500InternalServerError)]
         [HttpGet("attempt/{id}")]
         public async Task<IActionResult> GetAttemptInfo([Required, FromQuery] Guid id)
         {
-            return Ok();
+            var attempt = await _context.Attempts.FindAsync(id);
+
+            if (attempt == null) return NotFound(new ResponseModel("Error", "Can't find the attempt"));
+
+            var resAttempt = new AttemptModel(attempt);
+
+            return Ok(resAttempt);
         }
 
 
