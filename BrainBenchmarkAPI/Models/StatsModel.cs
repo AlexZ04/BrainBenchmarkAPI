7c85e71ece10675a9da622687a2aa2260ac365ee
Aleksey Zinovjev <alexz04tab2@gmail.com>
2025-01-25 22:57:56 +0700

    add stats model

---
+++ BrainBenchmarkAPI/Models/StatsModel.cs
@@ -0,0 +1,14 @@
+﻿namespace BrainBenchmarkAPI.Models
+{
+    public class StatsModel
+    {
+        public int AttemptsCounter { get; set; }
+        Dictionary<int, double> Results { get; set; } // ranged results : percentage result 
+
+        public StatsModel(int attemptsCounter, Dictionary<int, double> results)
+        {
+            AttemptsCounter = attemptsCounter;
+            Results = results;
+        }
+    }
+}
